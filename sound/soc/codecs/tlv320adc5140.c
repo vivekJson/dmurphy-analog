ac7e0c8120b4dae18c70894f55f0132e3379affa
Dan Murphy <dmurphy@ti.com>
2018-10-17 14:57:47 -0500

    ASoC: tlv320adc5140: Add the tlv320adc5140 codec driver
    Add the tlv320adc5140 codec driver.
    This is a test driver and is not complete in the mapping or the controls.
    There is an init hack which hard enables the 4 Analog microphones.
    
    THIS DRIVER SHOULD NOT BE USED FOR ANY PRODUCTION.
    
    There is also a debug file that allows the reading and writing of the registers.
    
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

---
+++ sound/soc/codecs/tlv320adc5140.c
@@ -0,0 +1,578 @@
+// SPDX-License-Identifier: GPL-2.0
+// TLV320ADC5140 Sound driver
+// Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/delay.h>
+#include <linux/pm.h>
+#include <linux/i2c.h>
+#include <linux/gpio/consumer.h>
+#include <linux/regulator/consumer.h>
+#include <linux/acpi.h>
+#include <linux/of.h>
+#include <linux/of_gpio.h>
+#include <linux/slab.h>
+#include <sound/core.h>
+#include <sound/pcm.h>
+#include <sound/pcm_params.h>
+#include <sound/soc.h>
+#include <sound/initval.h>
+#include <sound/tlv.h>
+
+#include "tlv320adc5140.h"
+
+#define TLV320ADC5140_REG_DEBUG 1
+
+#define ADC5140_RESET	BIT(0)
+
+#define ADC5140_BCLKINV_BIT	BIT(2)
+#define ADC5140_BCLK_FSYNC_MASTER	BIT(7)
+#define ADC5140_I2S_MODE_BIT	BIT(6)
+#define ADC5140_LEFT_JUST_BIT	BIT(7)
+
+#define ADC5140_20_BIT_WORD	BIT(4)
+#define ADC5140_24_BIT_WORD	BIT(5)
+#define ADC5140_32_BIT_WORD	(BIT(4) | BIT(5))
+#define ADC5140_WORD_LEN_MSK	0x30
+
+static const struct reg_default adc5140_reg_defaults[] = {
+	{ADC5140_PAGE_SELECT, 0x00},
+};
+
+static const struct regmap_range_cfg adc5140_ranges[] = {
+	{
+		.range_min = 0,
+		.range_max = 12 * 128,
+		.selector_reg = ADC5140_PAGE_SELECT,
+		.selector_mask = 0xff,
+		.selector_shift = 0,
+		.window_start = 0,
+		.window_len = 128,
+	},
+};
+
+static const struct regmap_config adc5140_i2c_regmap = {
+	.reg_bits = 8,
+	.val_bits = 8,
+	.reg_defaults = adc5140_reg_defaults,
+	.num_reg_defaults = ARRAY_SIZE(adc5140_reg_defaults),
+	.cache_type = REGCACHE_FLAT,
+	.ranges = adc5140_ranges,
+	.num_ranges = ARRAY_SIZE(adc5140_ranges),
+	.max_register = 12 * 128,
+};
+
+
+static const char * const mic_select_text[] = {
+	"2.5 kOhm", "10 kOhm", "20 kOhm"
+};
+
+static const DECLARE_TLV_DB_SCALE(adc_fgain_tlv, 0, 50, 0);
+
+static SOC_ENUM_SINGLE_DECL(mic1p_p_enum, ADC5140_CH1_CFG0, 2,
+	mic_select_text);
+static SOC_ENUM_SINGLE_DECL(mic2p_p_enum, ADC5140_CH2_CFG0, 2,
+	mic_select_text);
+static SOC_ENUM_SINGLE_DECL(mic3p_p_enum, ADC5140_CH3_CFG0, 2,
+	mic_select_text);
+static SOC_ENUM_SINGLE_DECL(mic4p_p_enum, ADC5140_CH4_CFG0, 2,
+	mic_select_text);
+static SOC_ENUM_SINGLE_DECL(mic1m_p_enum, ADC5140_CH1_CFG0, 2,
+	mic_select_text);
+static SOC_ENUM_SINGLE_DECL(mic2m_p_enum, ADC5140_CH2_CFG0, 2,
+	mic_select_text);
+static SOC_ENUM_SINGLE_DECL(mic3m_p_enum, ADC5140_CH3_CFG0, 2,
+	mic_select_text);
+static SOC_ENUM_SINGLE_DECL(mic4m_p_enum, ADC5140_CH4_CFG0, 2,
+	mic_select_text);
+
+static const struct snd_kcontrol_new p_term_mic1p =
+	SOC_DAPM_ENUM("MIC1P P-Terminal", mic1p_p_enum);
+static const struct snd_kcontrol_new p_term_mic2p =
+	SOC_DAPM_ENUM("MIC2P P-Terminal", mic2p_p_enum);
+static const struct snd_kcontrol_new p_term_mic3p =
+	SOC_DAPM_ENUM("MIC3P P-Terminal", mic3p_p_enum);
+static const struct snd_kcontrol_new p_term_mic4p =
+	SOC_DAPM_ENUM("MIC4P P-Terminal", mic4p_p_enum);
+static const struct snd_kcontrol_new p_term_mic1m =
+	SOC_DAPM_ENUM("MIC1M M-Terminal", mic1m_p_enum);
+static const struct snd_kcontrol_new p_term_mic2m =
+	SOC_DAPM_ENUM("MIC2M M-Terminal", mic2m_p_enum);
+static const struct snd_kcontrol_new p_term_mic3m =
+	SOC_DAPM_ENUM("MIC3M M-Terminal", mic3m_p_enum);
+static const struct snd_kcontrol_new p_term_mic4m =
+	SOC_DAPM_ENUM("MIC4M M-Terminal", mic4m_p_enum);
+
+static const struct snd_soc_dapm_widget adc5140_dapm_widgets[] = {
+	/* Inputs */
+	SND_SOC_DAPM_INPUT("MIC1"),
+	SND_SOC_DAPM_INPUT("MIC1P"),
+	SND_SOC_DAPM_INPUT("MIC1M"),
+	SND_SOC_DAPM_INPUT("MIC2P"),
+	SND_SOC_DAPM_INPUT("MIC2M"),
+	SND_SOC_DAPM_INPUT("MIC3P"),
+	SND_SOC_DAPM_INPUT("MIC3M"),
+	SND_SOC_DAPM_INPUT("MIC4P"),
+	SND_SOC_DAPM_INPUT("MIC4M"),
+
+	/* Input Selection to MIC_PGA */
+	SND_SOC_DAPM_MUX("MIC1P P-Terminal", SND_SOC_NOPM, 0, 0,
+			 &p_term_mic1p),
+	SND_SOC_DAPM_MUX("MIC2P P-Terminal", SND_SOC_NOPM, 0, 0,
+			 &p_term_mic2p),
+	SND_SOC_DAPM_MUX("MIC3P P-Terminal", SND_SOC_NOPM, 0, 0,
+			 &p_term_mic3p),
+	SND_SOC_DAPM_MUX("MIC4P P-Terminal", SND_SOC_NOPM, 0, 0,
+			 &p_term_mic4p),
+
+	SND_SOC_DAPM_MUX("MIC1M M-Terminal", SND_SOC_NOPM, 0, 0,
+			 &p_term_mic1m),
+	SND_SOC_DAPM_MUX("MIC2M M-Terminal", SND_SOC_NOPM, 0, 0,
+			 &p_term_mic2m),
+	SND_SOC_DAPM_MUX("MIC3M M-Terminal", SND_SOC_NOPM, 0, 0,
+			 &p_term_mic3m),
+	SND_SOC_DAPM_MUX("MIC4M M-Terminal", SND_SOC_NOPM, 0, 0,
+			 &p_term_mic4m),
+
+	/* Enabling & Disabling MIC Gain Ctl */
+	SND_SOC_DAPM_PGA("MIC_GAIN_CTL_CH1", ADC5140_CH1_CFG1,
+			 7, 1, NULL, 0),
+	SND_SOC_DAPM_PGA("MIC_GAIN_CTL_CH2", ADC5140_CH2_CFG1,
+			 7, 1, NULL, 0),
+	SND_SOC_DAPM_PGA("MIC_GAIN_CTL_CH3", ADC5140_CH3_CFG1,
+			 7, 1, NULL, 0),
+	SND_SOC_DAPM_PGA("MIC_GAIN_CTL_CH4", ADC5140_CH4_CFG1,
+			 7, 1, NULL, 0),
+	SND_SOC_DAPM_PGA("MIC_GAIN_CTL_CH5", ADC5140_CH5_CFG1,
+			 7, 1, NULL, 0),
+	SND_SOC_DAPM_PGA("MIC_GAIN_CTL_CH6", ADC5140_CH6_CFG1,
+			 7, 1, NULL, 0),
+	SND_SOC_DAPM_PGA("MIC_GAIN_CTL_CH7", ADC5140_CH7_CFG1,
+			 7, 1, NULL, 0),
+	SND_SOC_DAPM_PGA("MIC_GAIN_CTL_CH8", ADC5140_CH8_CFG1,
+			 7, 1, NULL, 0),
+
+	SND_SOC_DAPM_ADC("ADC", "Capture", 0, 0, 0),
+
+	SND_SOC_DAPM_AIF_OUT("AIF OUT", "Capture", 0, SND_SOC_NOPM, 0, 0),
+};
+
+static const struct snd_soc_dapm_route adc5140_audio_map[] = {
+	/* Mic input */
+	{"MIC1P P-Terminal", "2.5 kOhm", "MIC1P"},
+	{"MIC1P P-Terminal", "10 kOhm", "MIC1P"},
+	{"MIC1P P-Terminal", "20 kOhm", "MIC1P"},
+
+	{"MIC2P P-Terminal", "2.5 kOhm", "MIC2P"},
+	{"MIC2P P-Terminal", "10 kOhm", "MIC2P"},
+	{"MIC2P P-Terminal", "20 kOhm", "MIC2P"},
+
+	{"MIC3P P-Terminal", "2.5 kOhm", "MIC3P"},
+	{"MIC3P P-Terminal", "10 kOhm", "MIC3P"},
+	{"MIC3P P-Terminal", "20 kOhm", "MIC3P"},
+
+	{"MIC4P P-Terminal", "2.5 kOhm", "MIC4P"},
+	{"MIC4P P-Terminal", "10 kOhm", "MIC4P"},
+	{"MIC4P P-Terminal", "20 kOhm", "MIC4P"},
+
+	{"MIC1M M-Terminal", "2.5 kOhm", "MIC1M"},
+	{"MIC1M M-Terminal", "10 kOhm", "MIC1M"},
+	{"MIC1M M-Terminal", "20 kOhm", "MIC1M"},
+
+	{"MIC2M M-Terminal", "2.5 kOhm", "MIC2M"},
+	{"MIC2M M-Terminal", "10 kOhm", "MIC2M"},
+	{"MIC2M M-Terminal", "20 kOhm", "MIC2M"},
+
+	{"MIC3M M-Terminal", "2.5 kOhm", "MIC3M"},
+	{"MIC3M M-Terminal", "10 kOhm", "MIC3M"},
+	{"MIC3M M-Terminal", "20 kOhm", "MIC3M"},
+
+	{"MIC4M M-Terminal", "2.5 kOhm", "MIC4M"},
+	{"MIC4M M-Terminal", "10 kOhm", "MIC4M"},
+	{"MIC4M M-Terminal", "20 kOhm", "MIC4M"},
+
+	{"MIC_GAIN_CTL_CH1", NULL, "MIC1P P-Terminal"},
+	{"MIC_GAIN_CTL_CH3", NULL, "MIC2P P-Terminal"},
+	{"MIC_GAIN_CTL_CH5", NULL, "MIC3P P-Terminal"},
+	{"MIC_GAIN_CTL_CH7", NULL, "MIC4P P-Terminal"},
+	{"MIC_GAIN_CTL_CH2", NULL, "MIC1M M-Terminal"},
+	{"MIC_GAIN_CTL_CH4", NULL, "MIC2M M-Terminal"},
+	{"MIC_GAIN_CTL_CH6", NULL, "MIC3M M-Terminal"},
+	{"MIC_GAIN_CTL_CH8", NULL, "MIC4M M-Terminal"},
+
+	{"ADC", NULL, "MIC_GAIN_CTL_CH1"},
+	{"ADC", NULL, "MIC_GAIN_CTL_CH2"},
+	{"ADC", NULL, "MIC_GAIN_CTL_CH3"},
+	{"ADC", NULL, "MIC_GAIN_CTL_CH4"},
+	{"ADC", NULL, "MIC_GAIN_CTL_CH5"},
+	{"ADC", NULL, "MIC_GAIN_CTL_CH6"},
+	{"ADC", NULL, "MIC_GAIN_CTL_CH7"},
+	{"ADC", NULL, "MIC_GAIN_CTL_CH8"},
+
+	{"AIF OUT", NULL, "ADC"},
+
+	{"MIC1", NULL, "MIC1P P-Terminal"},
+	{"MIC1", NULL, "MIC1M M-Terminal"},
+};
+
+static const struct snd_kcontrol_new adc5140_snd_controls[] = {
+	SOC_SINGLE_TLV("Digital Channel 1 Capture Volume", ADC5140_CH1_CFG2,
+			4, 4, 1, adc_fgain_tlv),
+	SOC_SINGLE_TLV("Digital Channel 2 Capture Volume", ADC5140_CH2_CFG2,
+			4, 4, 1, adc_fgain_tlv),
+	SOC_SINGLE_TLV("Digital Channel 3 Capture Volume", ADC5140_CH3_CFG2,
+			4, 4, 1, adc_fgain_tlv),
+	SOC_SINGLE_TLV("Digital Channel 4 Capture Volume", ADC5140_CH4_CFG2,
+			4, 4, 1, adc_fgain_tlv),
+	SOC_SINGLE_TLV("Digital Channel 5 Capture Volume", ADC5140_CH5_CFG2,
+			4, 4, 1, adc_fgain_tlv),
+	SOC_SINGLE_TLV("Digital Channel 6 Capture Volume", ADC5140_CH6_CFG2,
+			4, 4, 1, adc_fgain_tlv),
+	SOC_SINGLE_TLV("Digital Channel 7 Capture Volume", ADC5140_CH7_CFG2,
+			4, 4, 1, adc_fgain_tlv),
+	SOC_SINGLE_TLV("Digital Channel 8 Capture Volume", ADC5140_CH8_CFG2,
+			4, 4, 1, adc_fgain_tlv),
+};
+
+static int adc5140_reset(struct adc5140_priv *adc5140)
+{
+	int ret;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_SW_RESET, ADC5140_RESET);
+
+	return ret;
+}
+
+static int adc5140_hw_params(struct snd_pcm_substream *substream,
+			     struct snd_pcm_hw_params *params,
+			     struct snd_soc_dai *dai)
+{
+	struct snd_soc_component *component = dai->component;
+	u8 data = 0;
+
+	dev_dbg(component->dev, "## %s: width %d rate %d\n",
+		__func__, params_width(params),
+		params_rate(params));
+
+	switch (params_width(params)) {
+	case 16:
+		break;
+	case 20:
+		data = ADC5140_20_BIT_WORD;
+		break;
+	case 24:
+		data = ADC5140_24_BIT_WORD;
+		break;
+	case 32:
+		data = ADC5140_32_BIT_WORD;
+		break;
+	default:
+		dev_err(component->dev, "%s: Unsupported width %d\n",
+			__func__, params_width(params));
+		return -EINVAL;
+	}
+
+	snd_soc_component_update_bits(component, ADC5140_ASI_CFG0,
+			    ADC5140_WORD_LEN_MSK, data);
+
+	return 0;
+}
+
+static int adc5140_set_dai_fmt(struct snd_soc_dai *codec_dai,
+			       unsigned int fmt)
+{
+	struct snd_soc_component *component = codec_dai->component;
+	u8 iface_reg1 = 0;
+	u8 iface_reg2 = 0;
+
+	dev_dbg(component->dev, "## %s: fmt = 0x%x\n", __func__, fmt);
+
+	/* set master/slave audio interface */
+	switch (fmt & SND_SOC_DAIFMT_MASTER_MASK) {
+	case SND_SOC_DAIFMT_CBM_CFM:
+		iface_reg2 |= ADC5140_BCLK_FSYNC_MASTER;
+		break;
+	case SND_SOC_DAIFMT_CBS_CFS:
+	case SND_SOC_DAIFMT_CBS_CFM:
+	case SND_SOC_DAIFMT_CBM_CFS:
+		break;
+	default:
+		dev_err(component->dev, "Invalid DAI master/slave interface\n");
+		return -EINVAL;
+	}
+
+	/* signal polarity */
+	switch (fmt & SND_SOC_DAIFMT_INV_MASK) {
+	case SND_SOC_DAIFMT_NB_NF:
+		break;
+	case SND_SOC_DAIFMT_IB_NF:
+		iface_reg1 |= ADC5140_BCLKINV_BIT;
+		break;
+	default:
+		dev_err(component->dev, "Invalid DAI clock signal polarity\n");
+		return -EINVAL;
+	}
+
+	/* interface format */
+	switch (fmt & SND_SOC_DAIFMT_FORMAT_MASK) {
+	case SND_SOC_DAIFMT_I2S:
+		iface_reg1 |= ADC5140_I2S_MODE_BIT;
+		break;
+	case SND_SOC_DAIFMT_LEFT_J:
+		iface_reg1 |= ADC5140_LEFT_JUST_BIT;
+		break;
+	default:
+		dev_err(component->dev, "Invalid DAI interface format\n");
+		return -EINVAL;
+	}
+
+	snd_soc_component_update_bits(component, ADC5140_ASI_CFG0,
+				      ADC5140_BCLKINV_BIT,
+				      iface_reg1);
+	snd_soc_component_update_bits(component, ADC5140_MST_CFG0,
+			    ADC5140_BCLK_FSYNC_MASTER,
+			    iface_reg2);
+
+	return 0;
+}
+
+static int adc5140_dac_mute(struct snd_soc_dai *codec_dai, int mute)
+{
+	struct snd_soc_component *component = codec_dai->component;
+
+	dev_dbg(component->dev, "## %s: mute = 0x%x\n", __func__, mute);
+
+	return 0;
+}
+
+static int adc5140_set_dai_sysclk(struct snd_soc_dai *codec_dai,
+				  int clk_id, unsigned int freq, int dir)
+{
+	struct snd_soc_component *component = codec_dai->component;
+	struct adc5140_priv *adc5140 = snd_soc_component_get_drvdata(component);
+	int i;
+
+	dev_dbg(component->dev, "## %s: clk_id = %d, freq = %d, dir = %d\n",
+		__func__, clk_id, freq, dir);
+
+	for (i = 1; i < 8; i++)
+		if (freq / i <= 20000000)
+			break;
+
+	if (freq/i > 20000000) {
+		dev_err(adc5140->dev, "%s: Too high mclk frequency %u\n",
+				__func__, freq);
+		return -EINVAL;
+	}
+
+	adc5140->p_div = i;
+	adc5140->sysclk = freq;
+
+	return 0;
+}
+
+static const struct snd_soc_dai_ops adc5140_dai_ops = {
+	.hw_params	= adc5140_hw_params,
+	.set_sysclk	= adc5140_set_dai_sysclk,
+	.set_fmt	= adc5140_set_dai_fmt,
+	.digital_mute	= adc5140_dac_mute,
+};
+
+static int adc5410_init_hack(struct adc5140_priv *adc5140)
+{
+	int ret;
+
+printk("%s: Enter\n", __func__);
+	ret = regmap_write(adc5140->regmap, ADC5140_SLEEP_CFG, 0x81);
+	if (ret)
+		goto out;
+
+	ret = adc5140_reset(adc5140);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_SLEEP_CFG, 0x81);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_ASI_CFG0, 0x30);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_CH1_CFG0, 0x4c);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_CH2_CFG0, 0x4c);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_CH3_CFG0, 0x4c);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_CH4_CFG0, 0x4c);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_IN_CH_EN, 0xf0);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_ASI_OUT_CH_EN, 0xf0);
+	if (ret)
+		goto out;
+
+	ret = regmap_write(adc5140->regmap, ADC5140_PWR_CFG, 0xe0);
+	if (ret)
+		goto out;
+
+out:
+	return ret;
+}
+
+static int adc5140_codec_probe(struct snd_soc_codec *codec)
+{
+	struct adc5140_priv *adc5140 = snd_soc_codec_get_drvdata(codec);
+	struct snd_soc_dapm_context *dapm = snd_soc_codec_get_dapm(codec);
+	int ret;
+
+	dev_dbg(adc5140->dev, "## %s\n", __func__);
+printk("%s: Enter\n", __func__);
+
+	regcache_cache_only(adc5140->regmap, true);
+	regcache_mark_dirty(adc5140->regmap);
+
+
+	snd_soc_add_codec_controls(codec, adc5140_snd_controls,
+			ARRAY_SIZE(adc5140_snd_controls));
+
+	ret = snd_soc_dapm_new_controls(dapm, adc5140_dapm_widgets,
+			ARRAY_SIZE(adc5140_dapm_widgets));
+	if (ret)
+		return ret;
+
+	ret = snd_soc_dapm_add_routes(dapm, adc5140_audio_map,
+				      ARRAY_SIZE(adc5140_audio_map));
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+static int adc5140_set_bias_level(struct snd_soc_codec *codec,
+				enum snd_soc_bias_level level)
+{
+	struct adc5140_priv *adc5140 = snd_soc_codec_get_drvdata(codec);
+
+
+	dev_dbg(adc5140->dev, "## %s:Enter\n", __func__);
+	switch (level) {
+	case SND_SOC_BIAS_ON:
+		break;
+	case SND_SOC_BIAS_PREPARE:
+		break;
+	case SND_SOC_BIAS_STANDBY:
+		break;
+	case SND_SOC_BIAS_OFF:
+		break;
+	}
+
+	return 0;
+}
+
+static int adc5140_codec_remove(struct snd_soc_codec *codec)
+{
+	dev_dbg(codec->dev, "## %s:Enter\n", __func__);
+	return 0;
+}
+
+static struct snd_soc_codec_driver soc_codec_driver_adc5140 = {
+	.probe			= adc5140_codec_probe,
+	.remove			= adc5140_codec_remove,
+	.controls		= adc5140_snd_controls,
+	.num_controls		= ARRAY_SIZE(adc5140_snd_controls),
+	.dapm_widgets		= adc5140_dapm_widgets,
+	.num_dapm_widgets	= ARRAY_SIZE(adc5140_dapm_widgets),
+	.dapm_routes		= adc5140_audio_map,
+	.num_dapm_routes	= ARRAY_SIZE(adc5140_audio_map),
+	.set_bias_level 	= adc5140_set_bias_level,
+};
+
+static struct snd_soc_dai_driver adc5140_dai_driver[] = {
+	{
+		.name = "tlv320adc5140-codec",
+		.capture = {
+			.stream_name	 = "Capture",
+			.channels_min	 = 2,
+			.channels_max	 = 8,
+			.rates		 = ADC5140_RATES,
+			.formats	 = ADC5140_FORMATS,
+		},
+		.ops = &adc5140_dai_ops,
+		.symmetric_rates = 1,
+	}
+};
+
+static const struct of_device_id tlv320adc5140_of_match[] = {
+	{ .compatible = "ti,tlv320adc5140" },
+	{},
+};
+MODULE_DEVICE_TABLE(of, tlv320adc5140_of_match);
+
+static int adc5140_i2c_probe(struct i2c_client *i2c,
+			     const struct i2c_device_id *id)
+{
+
+	struct adc5140_priv *adc5140;
+	int ret;
+
+	dev_dbg(&i2c->dev, "## %s: %s codec_type = %d\n", __func__,
+		id->name, (int)id->driver_data);
+
+	adc5140 = devm_kzalloc(&i2c->dev, sizeof(*adc5140), GFP_KERNEL);
+	if (!adc5140)
+		return -ENOMEM;
+
+	adc5140->regmap = devm_regmap_init_i2c(i2c, &adc5140_i2c_regmap);
+	if (IS_ERR(adc5140->regmap)) {
+		ret = PTR_ERR(adc5140->regmap);
+		dev_err(&i2c->dev, "Failed to allocate register map: %d\n",
+			ret);
+		return ret;
+	}
+	adc5140->dev = &i2c->dev;
+
+	i2c_set_clientdata(i2c, adc5140);
+#ifdef TLV320ADC5140_REG_DEBUG
+	/* For Debug only */
+	tlv320adc5140_init_debug(adc5140);
+#endif
+	adc5410_init_hack(adc5140);
+
+	return snd_soc_register_codec(&i2c->dev,
+				&soc_codec_driver_adc5140,
+				adc5140_dai_driver, 1);
+}
+
+static const struct i2c_device_id adc5140_i2c_id[] = {
+	{ "tlv320adc5140", 0 },
+	{}
+};
+MODULE_DEVICE_TABLE(i2c, adc5140_i2c_id);
+
+static struct i2c_driver adc5140_i2c_driver = {
+	.driver = {
+		.name	= "tlv320adc5140-codec",
+		.of_match_table = of_match_ptr(tlv320adc5140_of_match),
+	},
+	.probe		= adc5140_i2c_probe,
+	.id_table	= adc5140_i2c_id,
+};
+module_i2c_driver(adc5140_i2c_driver);
+
+MODULE_AUTHOR("Dan Murphy <dmurphy@ti.com>");
+MODULE_DESCRIPTION("ASoC TLV320ADC5140 CODEC Driver");
+MODULE_LICENSE("GPL v2");
---
+++ sound/soc/codecs/tlv320adc5140.h
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: GPL-2.0
+// TLV320ADC5104 Sound driver
+// Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+
+#ifndef _TLV320ADC5140_H
+#define _TLV320ADC5140_H
+
+#define ADC5140_RATES	SNDRV_PCM_RATE_8000_192000
+
+#define ADC5140_FORMATS	(SNDRV_PCM_FMTBIT_S16_LE | \
+			 SNDRV_PCM_FMTBIT_S20_3LE | \
+			 SNDRV_PCM_FMTBIT_S24_3LE | \
+			 SNDRV_PCM_FMTBIT_S24_LE | \
+			 SNDRV_PCM_FMTBIT_S32_LE)
+
+#define ADC5140_PAGE_SELECT	0x00
+#define ADC5140_SW_RESET	0x01
+#define ADC5140_SLEEP_CFG 	0x02
+#define ADC5140_SHDN_CFG	0x05
+#define ADC5140_ASI_CFG0	0x07
+#define ADC5140_ASI_CFG1	0x08
+#define ADC5140_ASI_CFG2	0x09
+#define ADC5140_ASI_CH1		0x0b
+#define ADC5140_ASI_CH2		0x0c
+#define ADC5140_ASI_CH3		0x0d
+#define ADC5140_ASI_CH4		0x0e
+#define ADC5140_ASI_CH5		0x0f
+#define ADC5140_ASI_CH6		0x10
+#define ADC5140_ASI_CH7		0x11
+#define ADC5140_ASI_CH8		0x12
+#define ADC5140_MST_CFG0	0x13
+#define ADC5140_MST_CFG1	0x14
+#define ADC5140_ASI_STS		0x15 
+#define ADC5140_PDMCLK_CFG	0x1f
+#define ADC5140_PDM_CFG		0x20
+#define ADC5140_GPIO_CFG0	0x21
+#define ADC5140_GPIO_CFG1	0x22
+#define ADC5140_GPIO_CFG2	0x23
+#define ADC5140_GPIO_CFG3	0x24
+#define ADC5140_GPIO_CFG4	0x25
+#define ADC5140_GPIO_VAL	0x29
+#define ADC5140_GPIO_MON	0x2a
+#define ADC5140_GPI_CFG0	0x2b
+#define ADC5140_GPI_CFG1	0x2c
+#define ADC5140_GPI_MON		0x2f
+#define ADC5140_INT_CFG		0x32
+#define ADC5140_INT_MASK0	0x33
+#define ADC5140_INT_LTCH0	0x36
+#define ADC5140_BIAS_CFG	0x3b
+#define ADC5140_CH1_CFG0	0x3c
+#define ADC5140_CH1_CFG1	0x3d
+#define ADC5140_CH1_CFG2	0x3e
+#define ADC5140_CH1_CFG3	0x3f
+#define ADC5140_CH1_CFG4	0x40
+#define ADC5140_CH2_CFG0	0x41
+#define ADC5140_CH2_CFG1	0x42
+#define ADC5140_CH2_CFG2	0x43
+#define ADC5140_CH2_CFG3	0x44
+#define ADC5140_CH2_CFG4	0x45
+#define ADC5140_CH3_CFG0	0x46
+#define ADC5140_CH3_CFG1	0x47
+#define ADC5140_CH3_CFG2	0x48
+#define ADC5140_CH3_CFG3	0x49
+#define ADC5140_CH3_CFG4 	0x4a
+#define ADC5140_CH4_CFG0	0x4b
+#define ADC5140_CH4_CFG1	0x4c
+#define ADC5140_CH4_CFG2	0x4d
+#define ADC5140_CH4_CFG3	0x4e
+#define ADC5140_CH4_CFG4	0x4f
+#define ADC5140_CH5_CFG0	0x50
+#define ADC5140_CH5_CFG1	0x51
+#define ADC5140_CH5_CFG2	0x52
+#define ADC5140_CH5_CFG3	0x53
+#define ADC5140_CH5_CFG4	0x54
+#define ADC5140_CH6_CFG0	0x55
+#define ADC5140_CH6_CFG1	0x56
+#define ADC5140_CH6_CFG2	0x57
+#define ADC5140_CH6_CFG3	0x58
+#define ADC5140_CH6_CFG4	0x59
+#define ADC5140_CH7_CFG0	0x5a
+#define ADC5140_CH7_CFG1	0x5b
+#define ADC5140_CH7_CFG2	0x5c
+#define ADC5140_CH7_CFG3	0x5d
+#define ADC5140_CH7_CFG4	0x5e
+#define ADC5140_CH8_CFG0	0x5f
+#define ADC5140_CH8_CFG1	0x60
+#define ADC5140_CH8_CFG2	0x61
+#define ADC5140_CH8_CFG3	0x62
+#define ADC5140_CH8_CFG4	0x63
+#define ADC5140_DSP_CFG0	0x6b
+#define ADC5140_DSP_CFG1	0x6c
+#define ADC5140_DRE_CFG0	0x6d
+#define ADC5140_IN_CH_EN	0x73
+#define ADC5140_ASI_OUT_CH_EN	0x74
+#define ADC5140_PWR_CFG		0x75
+#define ADC5140_DEV_STS0	0x76
+#define ADC5140_DEV_STS1	0x77
+
+struct adc5140_priv {
+	struct snd_soc_component *component;
+	struct gpio_desc *gpio_reset;
+	struct regmap *regmap;
+	struct device *dev;
+
+	int micbias_vg;
+	unsigned int sysclk;
+	u8 p_div;
+};
+
+int tlv320adc5140_init_debug(struct adc5140_priv* adc5140);
+
+#endif /* _TLV320ADC5140_ */
